eff2604b3610118f8302d256ab8f5e3deebd7a76
LucasRios95 <lucashrios95@gmail.com>
2024-01-24 17:01:46 -0300

    deletar conta bancária add

--- src/modules/BankAccount/useCases/deleteBankAccount/DeleteBankAccountController.ts
+++ src/modules/BankAccount/useCases/deleteBankAccount/DeleteBankAccountController.ts
@@ -6,6 +6,7 @@ import { DeleteBankAccountUseCase } from "./DeleteBankAccountUseCase";
 class DeleteBankAccountController {
     async handle(request: Request, response: Response): Promise<Response> {
         const { account_number } = request.params;
+        
         const deleteBankAccountUsecase = container.resolve(DeleteBankAccountUseCase);
 
         await deleteBankAccountUsecase.execute(account_number);
--- src/shared/infra/http/routes/bankAccounts.routes.ts
+++ src/shared/infra/http/routes/bankAccounts.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { CreateBankAccountController } from "modules/BankAccount/useCases/createBankAccount/CreateBankAccountController";
+import { DeleteBankAccountController } from "modules/BankAccount/useCases/deleteBankAccount/DeleteBankAccountController";
 import { ListBankAccountController } from "modules/BankAccount/useCases/listBankAccount/ListBankAccountController";
 
 const bankAccountRoutes = Router();
 
 const createBankAccountController = new CreateBankAccountController();
 const listBankAccountController = new ListBankAccountController();
+const deleteBankAccountController = new DeleteBankAccountController();
 
 bankAccountRoutes.post("/", createBankAccountController.handle);
 bankAccountRoutes.get("/", listBankAccountController.handle);
+bankAccountRoutes.delete("/:account_number", deleteBankAccountController.handle);
+
 
 export { bankAccountRoutes };
